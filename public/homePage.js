a2d9f34a695c4a4978533dd1f268158bfad74607
Aleksandr-Ljabin <frontdevell@gmail.com>
2021-10-29 12:56:24 +0300

    Исправление работы

--- public/homePage.js
+++ public/homePage.js
@@ -2,17 +2,17 @@
 let logoutButton = new LogoutButton();
 
 logoutButton.action = () => {
-	ApiConnector.logout((callback) => {
-        if (callback.success) {
+	ApiConnector.logout((response) => {
+        if (response.success) {
             location.reload();
         } 
     });
 };
 
 
-let current = ApiConnector.current((callback) => {
-    if (callback.success){
-        ProfileWidget.showProfile(callback.data)
+let current = ApiConnector.current((response) => {
+    if (response.success){
+        ProfileWidget.showProfile(response.data)
     }
 });
 
@@ -20,51 +20,44 @@ let current = ApiConnector.current((callback) => {
 
 let ratesBoard = new RatesBoard();
 function request(){
-    ApiConnector.getStocks((callback) => {
-        if (callback.success){
+    ApiConnector.getStocks((response) => {
+        if (response.success){
             ratesBoard.clearTable();
-            ratesBoard.fillTable(callback.data);
+            ratesBoard.fillTable(response.data);
         }
     })
 }
-
-setInterval(request(), 60000);
+request();
+setInterval(request, 60000);
 
 
 //Операции с деньгами
 //Пополнение баланса
 let moneyManager = new MoneyManager();
 moneyManager.addMoneyCallback = (data) => {
-    ApiConnector.addMoney(data, (callback) => {
-        if (callback.success){
-            ProfileWidget.showProfile(callback.data);
-            moneyManager.setMessage("Баланс пополнен");//не понятно как работает
-        } else{
-            moneyManager.setMessage();
-        }
+    ApiConnector.addMoney(data, (response) => {
+        if (response.success){
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, response.error || "Баланс пополнен");
+        } 
     })
 }
 
 //Конвертация
 moneyManager.conversionMoneyCallback = (data) => {
-    ApiConnector.convertMoney(data, (callback) => {
-        if (callback.success){
-            ProfileWidget.showProfile(callback.data);
-            moneyManager.setMessage("Баланс пополнен");//не понятно как работает
-        } else{
-            alert("Ошибка пополнения баланса")
-        }
+    ApiConnector.convertMoney(data, (response) => {
+        if (response.success){
+            ProfileWidget.showProfile(response.data);
+        }   moneyManager.setMessage(response.success, response.error || "Баланс пополнен");
     })
 }
 
 //Перевод
 moneyManager.sendMoneyCallback = (data) => {
-    ApiConnector.transferMoney(data, (callback) => {
-        if (callback.success){
-            ProfileWidget.showProfile(callback.data);
-            setMessage(success, "Баланс пополнен");//не понятно как работает
-        } else{
-            alert("Ошибка пополнения баланса")
+    ApiConnector.transferMoney(data, (response) => {
+        if (response.success){
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, response.error || "Баланс пополнен");
         }
     })
 }
@@ -73,33 +66,35 @@ moneyManager.sendMoneyCallback = (data) => {
 
 let favoritesWidget = new FavoritesWidget();
 function requestFavorites (){
-    ApiConnector.getFavorites((callback) => {
-        if (callback.success){
+    ApiConnector.getFavorites((response) => {
+        if (response.success){
             favoritesWidget.clearTable();
-            favoritesWidget.fillTable(callback.data);
-            moneyManager.updateUsersList(callback.data);
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
         }   
     })
 }
 requestFavorites();
 
-
+// Добавление в список избранных
 favoritesWidget.addUserCallback = (data) => {
-    ApiConnector.addUserToFavorites(data, (callback) => {
-        if (callback.success){
+    ApiConnector.addUserToFavorites(data, (response) => {
+        if (response.success){
             favoritesWidget.clearTable();
-            favoritesWidget.fillTable(callback.data);
-            moneyManager.updateUsersList(callback.data);
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, response.error || "Пользователь добавлен");
         } 
     })
 }
 
 favoritesWidget.removeUserCallback = (data) => {
-    ApiConnector.removeUserFromFavorites(data, (callback) => {
-        if (callback.success){
+    ApiConnector.removeUserFromFavorites(data, (response) => {
+        if (response.success){
             favoritesWidget.clearTable();
-            favoritesWidget.fillTable(callback.data);
-            moneyManager.updateUsersList(callback.data);
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, response.error || "Пользователь удален");
         } 
     })
 }
--- public/loginPage.js
+++ public/loginPage.js
@@ -4,21 +4,21 @@
 let userForm = new UserForm();
 
 userForm.loginFormCallback = (data) => {
-    ApiConnector.login(data, (callback) => {
-        if (callback.success) {
+    ApiConnector.login(data, (response) => {
+        if (response.success) {
             location.reload();
         } else {
-            userForm.setLoginErrorMessage(`Не правильно введены логин или пароль: ${callback.error}`);
+            userForm.setLoginErrorMessage(response.error);
         }
     });
 };
 
 userForm.registerFormCallback = (data) => {
-    ApiConnector.register(data, (callback) => {
-        if (callback.success) {
+    ApiConnector.register(data, (response) => {
+        if (response.success) {
             location.reload();
         } else {
-            userForm.setRegisterErrorMessage(`Ошибка регистрации нового пользователя ${data.login}: ${callback.error}`);
+            userForm.setRegisterErrorMessage(response.error);
         }
     });
 }
